f631060a19fe65f0e5521f0d3d9c77fd2233cbf0
hanif.robbani <dev.har07@gmail.com>
2023-05-23 16:53:19 +0700

    add project description in pypi

--- setup.py
+++ setup.py
@@ -1,15 +1,32 @@
 from distutils.core import setup
 
+long_desc = """Helper package for requesting token and generating request signatures to be used for consuming Bank Mandiri API.
+
+Example usage:
+
+```python
+from MandiriUtils import Client
+
+client = Client(url="https://sandbox.bankmandiri.co.id", pk=my_private_key_content, pk_pass=pk_pass, client_id=client_id, client_secret=client_secret, tz_offset="+0700")
+result = client.get_token()
+
+print(result)
+```
+"""
+
 setup(
   name = 'MandiriUtils',         
   packages = ['MandiriUtils'],   
-  version = '0.10',      
+  version = '0.11',      
   license='MIT',        
   description = 'Helper package for requesting token and generating request signatures to be used for consuming Bank Mandiri API',
+  long_description = 'Helper package for requesting token and generating request signatures to be used for consuming Bank Mandiri API',
+  long_description_content_type='text/markdown',
+
   author = 'Hanif Amal Robbani',       
   author_email = 'dev.har07@gmail.com',
   url = 'https://github.com/har07',
-  download_url = 'https://github.com/har07/mandiri-utils/archive/refs/tags/v_010.tar.gz', 
+  download_url = 'https://github.com/har07/mandiri-utils/archive/refs/tags/v_011.tar.gz', 
   keywords = ['banking', 'signature', 'token', 'mandiri'], 
   install_requires=[
           'pycryptodome==3.17.0',
